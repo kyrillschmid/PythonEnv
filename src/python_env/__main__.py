be19e42e40147f62d0d0dd3f22b2065d21ad45c3
Kyrill Schmid <kyrillschmid@web.de>
2024-04-10 17:00:10 +0200

    first commit

---
+++ src/python_env/__main__.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    print("hello world")
+    return 2
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/my_test.py
@@ -0,0 +1,5 @@
+from python_env.__main__ import main
+
+
+def test_main():
+    assert main() == 2
